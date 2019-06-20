77330eff70f80fdd15361ff6a4f4ec855c412281
leehncg <leehncg@gmail.com>
2019-06-20 22:01:43 +0800

    grid item 1/2

--- lib/widget/grid_nav.dart
+++ lib/widget/grid_nav.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_ctrip/model/common_model.dart';
 import 'package:flutter_ctrip/model/grid_nav_model.dart';
+import 'package:flutter_ctrip/widget/webview.dart';
 
 class GridNav extends StatelessWidget {
   final GridNavModel gridNavModel;
@@ -15,20 +18,98 @@ class GridNav extends StatelessWidget {
 
   _gridNavItems(BuildContext context) {
     List<Widget> items = [];
-    if(gridNavModel == null) return items;
-    if(gridNavModel.hotel!=null){
-
-    }
-    if(gridNavModel.flight!=null){
-
-    }
-    if(gridNavModel.travel!=null){
-
-    }
+    if (gridNavModel == null) return items;
+    if (gridNavModel.hotel != null) {}
+    if (gridNavModel.flight != null) {}
+    if (gridNavModel.travel != null) {}
   }
 
-  _gridNavItem(BuildContext context, GridNavItem gridNavItem, bool first){
+  _gridNavItem(BuildContext context, GridNavItem gridNavItem, bool first) {
     List<Widget> items = [];
+    items.add(_mainItem(context, gridNavItem.mainItem));
+    items.add(_doubleItem(context, gridNavItem.item1, gridNavItem.item2));
+    items.add(_doubleItem(context, gridNavItem.item3, gridNavItem.item4));
+    List<Widget> expandItems = [];
+    items.forEach((item) {
+      expandItems.add(Expanded(
+        child: item,
+        flex: 1,
+      ));
+    });
   }
 
+  _mainItem(BuildContext context, CommonModel model) {
+    return _warpGesture(
+        context,
+        Stack(
+          alignment: AlignmentDirectional.topCenter,
+          children: <Widget>[
+            Image.network(
+              model.icon,
+              fit: BoxFit.contain,
+              height: 88,
+              width: 121,
+              alignment: AlignmentDirectional.bottomEnd,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 11),
+              child: Text(
+                model.title,
+                style: TextStyle(fontSize: 14, color: Colors.white),
+              ),
+            )
+          ],
+        ),
+        model);
+  }
+
+  _doubleItem(BuildContext context, CommonModel topItem, CommonModel bottomItem) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: _item(context, topItem, true),
+        ),
+        Expanded(
+          child: _item(context, bottomItem, false),
+        )
+      ],
+    );
+  }
+
+  _item(BuildContext context, CommonModel item, bool first) {
+    BorderSide borderSide = BorderSide(width: 0.8, color: Colors.white);
+    return FractionallySizedBox(
+      widthFactor: 1,
+      child: Container(
+        decoration: BoxDecoration(
+            border: Border(
+          left: borderSide,
+          bottom: first ? borderSide : BorderSide.none,
+        )),
+        child: Center(
+          child: Text(
+            item.title,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 14, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+
+  _warpGesture(BuildContext context, Widget widget, CommonModel model) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => WebView(
+                    url: model.url,
+                    statusBarColor: model.statusBarColor,
+                    title: model.title,
+                    hideAppBar: model.hideAppBar)));
+      },
+      child: widget,
+    );
+  }
 }
